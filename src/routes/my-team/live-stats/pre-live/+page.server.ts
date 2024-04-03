30187a6e387a711d9df031a7276931a5486c1a2e
MBwilson <mckennon97@gmail.com>
2024-04-03 17:30:19 -0500

    Player Id's now stored in array in pre-live page and .server

--- src/routes/my-team/live-stats/pre-live/+page.server.ts
+++ src/routes/my-team/live-stats/pre-live/+page.server.ts
@@ -36,36 +36,17 @@ export const actions = {
 		// Wait for form data
 		const data = await request.formData();
 
-		// Extract data from form
 		const homeTeamId = BigInt(data.get('homeTeamId').toString());
-		const home_p1Id = BigInt(data.get('home_p1').toString());
-		const home_p2Id = BigInt(data.get('home_p2').toString());
-		const home_p3Id = BigInt(data.get('home_p3').toString());
-		const home_p4Id = BigInt(data.get('home_p4').toString());
-		const home_p5Id = BigInt(data.get('home_p5').toString());
-		const home_p6Id = BigInt(data.get('home_p6').toString());
-		const home_p7Id = BigInt(data.get('home_p7').toString());
-		const home_p8Id = BigInt(data.get('home_p8').toString());
-		const home_p9Id = BigInt(data.get('home_p9').toString());
-		const home_p10Id = BigInt(data.get('home_p10').toString());
-
-
 		const awayTeamId = BigInt(data.get('awayTeamId').toString());
-		const away_p1Id = BigInt(data.get('away_p1').toString());
-		const away_p2Id = BigInt(data.get('away_p2').toString());
-		const away_p3Id = BigInt(data.get('away_p3').toString());
-		const away_p4Id = BigInt(data.get('away_p4').toString());
-		const away_p5Id = BigInt(data.get('away_p5').toString());
-		const away_p6Id = BigInt(data.get('away_p6').toString());
-		const away_p7Id = BigInt(data.get('away_p7').toString());
-		const away_p8Id = BigInt(data.get('away_p8').toString());
-		const away_p9Id = BigInt(data.get('away_p9').toString());
-		const away_p10Id = BigInt(data.get('away_p10').toString());
-
 		// Array of home team players
-		const homeTeamPlayers = [home_p1Id, home_p2Id, home_p3Id, home_p4Id, home_p5Id, home_p6Id, home_p7Id, home_p8Id, home_p9Id, home_p10Id];
+		const homeTeamPlayers_ids = data.get('homePlayersIds');
+
 		// Array of away team players
-		const awayTeamPlayers = [away_p1Id, away_p2Id, away_p3Id, away_p4Id, away_p5Id, away_p6Id, away_p7Id, away_p8Id, away_p9Id, away_p10Id];
+		const awayTeamPlayers_ids = data.get('awayPlayersIds');
+
+		// Log collected form data in console
+		console.log("homeTeamPlayers:",homeTeamPlayers_ids);
+		console.log("awayTeamPlayers:",awayTeamPlayers_ids);
 
 		// Log collected form data in console
 		console.log("homeTeamId:",homeTeamId);
--- src/routes/my-team/live-stats/pre-live/+page.svelte
+++ src/routes/my-team/live-stats/pre-live/+page.svelte
@@ -41,27 +41,9 @@
         awayTeam_players = allPlayers[selectedAwayTeam - 1];
     };
 
-    let home_p1 = '';
-    let home_p2 = '';
-    let home_p3 = '';
-    let home_p4 = '';
-    let home_p5 = '';
-    let home_p6 = '';
-    let home_p7 = '';
-    let home_p8 = '';
-    let home_p9 = '';
-    let home_p10 = '';
+    let homeTeam_playerIds = ['', '', '', '', '', '', '', '', '', ''];
+    let awayTeam_playerIds = ['', '', '', '', '', '', '', '', '', ''];
 
-    let away_p1 = '';
-    let away_p2 = '';
-    let away_p3 = '';
-    let away_p4 = '';
-    let away_p5 = '';
-    let away_p6 = '';
-    let away_p7 = '';
-    let away_p8 = '';
-    let away_p9 = '';
-    let away_p10 = '';
 
     const handleHomePlayerSelect = (event) => {
         if (event.target.id == "home_p1") {
@@ -145,7 +127,7 @@
 
         
         <div><label for="homeP1Select">Player 1:</label>
-            <select bind:value={home_p1} on:change={handleHomePlayerSelect} required>
+            <select bind:value={homeTeam_playerIds[0]} on:change={handleHomePlayerSelect} required>
                 <option value="">Select Player</option>
                 {#each homeTeam_players as player}
                     <option value={player.player_id}>{player.last_name}</option>
@@ -153,7 +135,7 @@
             </select>
 
             <label for="homeP2Select">Player 2:</label>
-            <select bind:value={home_p2} on:change={handleHomePlayerSelect} required>
+            <select bind:value={homeTeam_playerIds[1]} on:change={handleHomePlayerSelect} required>
                 <option value="">Select Player</option>
                 {#each homeTeam_players as player}
                     <option value={player.player_id}>{player.last_name}</option>
@@ -161,7 +143,7 @@
             </select></div>
 
             <div> <label for="homeP3Select">Player 3:</label>
-            <select bind:value={home_p3} on:change={handleHomePlayerSelect} required>
+            <select bind:value={homeTeam_playerIds[2]} on:change={handleHomePlayerSelect} required>
                 <option value="">Select Player</option>
                 {#each homeTeam_players as player}
                     <option value={player.player_id}>{player.last_name}</option>
@@ -169,7 +151,7 @@
             </select>
 
             <label for="homeP4Select">Player 4:</label>
-            <select bind:value={home_p4} on:change={handleHomePlayerSelect} required>
+            <select bind:value={homeTeam_playerIds[3]} on:change={handleHomePlayerSelect} required>
                 <option value="">Select Player</option>
                 {#each homeTeam_players as player}
                     <option value={player.player_id}>{player.last_name}</option>
@@ -177,7 +159,7 @@
             </select></div>
 
             <div><label for="homeP5Select">Player 5:</label>
-            <select bind:value={home_p5} on:change={handleHomePlayerSelect} required>
+            <select bind:value={homeTeam_playerIds[4]} on:change={handleHomePlayerSelect} required>
                 <option value="">Select Player</option>
                 {#each homeTeam_players as player}
                     <option value={player.player_id}>{player.last_name}</option>
@@ -185,7 +167,7 @@
             </select>
 
             <label for="homeP6Select">Player 6:</label>
-            <select bind:value={home_p6} on:change={handleHomePlayerSelect} required>
+            <select bind:value={homeTeam_playerIds[5]} on:change={handleHomePlayerSelect} required>
                 <option value="">Select Player</option>
                 {#each homeTeam_players as player}
                     <option value={player.player_id}>{player.last_name}</option>
@@ -193,7 +175,7 @@
             </select></div>
 
             <div><label for="homeP7Select">Player 7:</label>
-            <select bind:value={home_p7} on:change={handleHomePlayerSelect} required>
+            <select bind:value={homeTeam_playerIds[6]} on:change={handleHomePlayerSelect} required>
                 <option value="">Select Player</option>
                 {#each homeTeam_players as player}
                     <option value={player.player_id}>{player.last_name}</option>
@@ -201,7 +183,7 @@
             </select>
 
             <label for="homeP8Select">Player 8:</label>
-            <select bind:value={home_p8} on:change={handleHomePlayerSelect} required>
+            <select bind:value={homeTeam_playerIds[7]} on:change={handleHomePlayerSelect} required>
                 <option value="">Select Player</option>
                 {#each homeTeam_players as player}
                     <option value={player.player_id}>{player.last_name}</option>
@@ -209,7 +191,7 @@
             </select></div>
 
             <div><label for="homeP9Select">Player 9:</label>
-            <select bind:value={home_p9} on:change={handleHomePlayerSelect} required>
+            <select bind:value={homeTeam_playerIds[8]} on:change={handleHomePlayerSelect} required>
                 <option value="">Select Player</option>
                 {#each homeTeam_players as player}
                     <option value={player.player_id}>{player.last_name}</option>
@@ -217,7 +199,7 @@
             </select>
 
             <label for="homeP10Select">Player 10:</label>
-            <select bind:value={home_p10} on:change={handleHomePlayerSelect} required>
+            <select bind:value={homeTeam_playerIds[9]} on:change={handleHomePlayerSelect} required>
                 <option value="">Select Player</option>
                 {#each homeTeam_players as player}
                     <option value={player.player_id}>{player.last_name}</option>
@@ -237,7 +219,7 @@
 
         <div>
         <label for="awayP1Select">Player 1:</label>
-        <select bind:value={away_p1} on:change={handleAwayPlayerSelect} required>
+        <select bind:value={awayTeam_playerIds[0]} on:change={handleAwayPlayerSelect} required>
             <option value="">Select Player</option>
             {#each awayTeam_players as player}
                 <option value={player.player_id}>{player.last_name}</option>
@@ -245,7 +227,7 @@
         </select>
 
         <label for="awayP2Select">Player 2:</label>
-        <select bind:value={away_p2} on:change={handleAwayPlayerSelect} required>
+        <select bind:value={awayTeam_playerIds[1]} on:change={handleAwayPlayerSelect} required>
             <option value="">Select Player</option>
             {#each awayTeam_players as player}
                 <option value={player.player_id}>{player.last_name}</option>
@@ -253,7 +235,7 @@
         </select></div>
 
         <div> <label for="awayP3Select">Player 3:</label>
-        <select bind:value={away_p3} on:change={handleAwayPlayerSelect} required>
+        <select bind:value={awayTeam_playerIds[2]} on:change={handleAwayPlayerSelect} required>
             <option value="">Select Player</option>
             {#each awayTeam_players as player}
                 <option value={player.player_id}>{player.last_name}</option>
@@ -261,7 +243,7 @@
         </select>
 
         <label for="awayP4Select">Player 4:</label>
-        <select bind:value={away_p4} on:change={handleAwayPlayerSelect} required>
+        <select bind:value={awayTeam_playerIds[3]} on:change={handleAwayPlayerSelect} required>
             <option value="">Select Player</option>
             {#each awayTeam_players as player}
                 <option value={player.player_id}>{player.last_name}</option>
@@ -269,7 +251,7 @@
         </select></div>
 
         <div><label for="awayP5Select">Player 5:</label>
-        <select bind:value={away_p5} on:change={handleAwayPlayerSelect} required>
+        <select bind:value={awayTeam_playerIds[4]} on:change={handleAwayPlayerSelect} required>
             <option value="">Select Player</option>
             {#each awayTeam_players as player}
                 <option value={player.player_id}>{player.last_name}</option>
@@ -277,7 +259,7 @@
         </select>
 
         <label for="awayP6Select">Player 6:</label>
-        <select bind:value={away_p6} on:change={handleAwayPlayerSelect} required>
+        <select bind:value={awayTeam_playerIds[5]} on:change={handleAwayPlayerSelect} required>
             <option value="">Select Player</option>
             {#each awayTeam_players as player}
                 <option value={player.player_id}>{player.last_name}</option>
@@ -285,7 +267,7 @@
         </select></div>
 
         <div><label for="awayP7Select">Player 7:</label>
-        <select bind:value={away_p7} on:change={handleAwayPlayerSelect} required>
+        <select bind:value={awayTeam_playerIds[6]} on:change={handleAwayPlayerSelect} required>
             <option value="">Select Player</option>
             {#each awayTeam_players as player}
                 <option value={player.player_id}>{player.last_name}</option>
@@ -293,7 +275,7 @@
         </select>
 
         <label for="awayP8Select">Player 8:</label>
-        <select bind:value={away_p8} on:change={handleAwayPlayerSelect} required>
+        <select bind:value={awayTeam_playerIds[7]} on:change={handleAwayPlayerSelect} required>
             <option value="">Select Player</option>
             {#each awayTeam_players as player}
                 <option value={player.player_id}>{player.last_name}</option>
@@ -301,7 +283,7 @@
         </select></div>
 
         <div><label for="awayP9Select">Player 9:</label>
-        <select bind:value={away_p9} on:change={handleAwayPlayerSelect} required>
+        <select bind:value={awayTeam_playerIds[8]} on:change={handleAwayPlayerSelect} required>
             <option value="">Select Player</option>
             {#each awayTeam_players as player}
                 <option value={player.player_id}>{player.last_name}</option>
@@ -309,7 +291,7 @@
         </select>
 
         <label for="awayP10Select">Player 10:</label>
-        <select bind:value={away_p10} on:change={handleAwayPlayerSelect} required>
+        <select bind:value={awayTeam_playerIds[9]} on:change={handleAwayPlayerSelect} required>
             <option value="">Select Player</option>
             {#each awayTeam_players as player}
                 <option value={player.player_id}>{player.last_name}</option>
@@ -327,27 +309,9 @@
     </div>
 
     <input type="hidden" name="homeTeamId" value={selectedHomeTeam}>
-    <input type="hidden" name="home_p1" value={home_p1}>
-    <input type="hidden" name="home_p2" value={home_p2}>
-    <input type="hidden" name="home_p3" value={home_p3}>
-    <input type="hidden" name="home_p4" value={home_p4}>
-    <input type="hidden" name="home_p5" value={home_p5}>
-    <input type="hidden" name="home_p6" value={home_p6}>
-    <input type="hidden" name="home_p7" value={home_p7}>
-    <input type="hidden" name="home_p8" value={home_p8}>
-    <input type="hidden" name="home_p9" value={home_p9}>
-    <input type="hidden" name="home_p10" value={home_p10}>
-
     <input type="hidden" name="awayTeamId" value={selectedAwayTeam}>
-    <input type="hidden" name="away_p1" value={away_p1}>
-    <input type="hidden" name="away_p2" value={away_p2}>
-    <input type="hidden" name="away_p3" value={away_p3}>
-    <input type="hidden" name="away_p4" value={away_p4}>
-    <input type="hidden" name="away_p5" value={away_p5}>
-    <input type="hidden" name="away_p6" value={away_p6}>
-    <input type="hidden" name="away_p7" value={away_p7}>
-    <input type="hidden" name="away_p8" value={away_p8}>
-    <input type="hidden" name="away_p9" value={away_p9}>
-    <input type="hidden" name="away_p10" value={away_p10}>
+
+    <input type="hidden" name="homePlayersIds" value={homeTeam_playerIds} required>
+    <input type="hidden" name="awayPlayersIds" value={awayTeam_playerIds} required>
 </form>
 
