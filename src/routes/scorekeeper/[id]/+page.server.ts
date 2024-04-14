74a838bbc728703db67c1836cf3d7fb647b8755d
Jesse Rheal <jessecolerheal@live.com>
2024-04-14 15:34:13 -0500

    Required team now must be selected in the pre live page.

--- src/routes/scorekeeper/[id]/+page.server.ts
+++ src/routes/scorekeeper/[id]/+page.server.ts
@@ -1,5 +1,5 @@
 import { goto } from '$app/navigation';
-import { getGame, getRowsFromVals, getScorebookSessionFromRoomCode, insertPlayerStats } from '$lib/db.js';
+import { getCoach, getGame, getRowsFromVals, getScorebookSessionFromRoomCode, insertPlayerStats } from '$lib/db.js';
 import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
@@ -7,11 +7,13 @@ export async function load({ locals, params }) {
     const roomCode = params.id;
     try {
 		const session = await getScorebookSessionFromRoomCode(roomCode);
+        const coach = await getCoach(session.coach_id);
         let game = (await getGame(Number(session.game_id))) as Game;
         return {
             props: {
                 locals,
-                game: game
+                game: game,
+                requiredTeam: coach.team_id
             }
         }
     }
